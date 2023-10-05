e9481e4418a0baa7d9320b2355ff1ff024df9827
ddmuzyk <domino2k1@gmail.com>
2023-10-05 18:42:15 +0200

    adding js to control the menu

--- src/script/script.js
+++ src/script/script.js
@@ -1,7 +1,25 @@
+import menuSvg from '../../public/svg/menu.svg';
+import xSvg from '../../public/svg/x.svg';
+
+
 const checkbox = document.getElementById('check');
+const menu = document.getElementById('menu');
+
+// Template for the menu source
+const menuTemplate = '/public/svg/';
 
 // Function to toggle the checkbox
 
 const toggleCheckbox = () => {
-  checkbox.checked = !checkbox.checked;
+  if (checkbox.checked) {
+    checkbox.checked = false;
+    menu.src = menuSvg;
+  } else {
+    menu.src = xSvg;
+  }
+
 }
+
+const toggler = document.querySelector('.toggler');
+
+toggler.addEventListener('click', toggleCheckbox);
