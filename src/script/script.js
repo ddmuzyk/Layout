4b79319f631f14f28e36837c66dae0cfc0aaca62
ddmuzyk <domino2k1@gmail.com>
2023-10-07 18:04:06 +0200

    fixed the css to match the design

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,6 @@
 import menuSvg from '../../public/svg/menu.svg';
 import xSvg from '../../public/svg/x.svg';
 
-// Remove the checkbox input from the DOM, use a variable instead
-
 // Global variables
 let menuOpen = false;
 
@@ -10,13 +8,14 @@ const checkbox = document.querySelector('.checkbtn');
 const menu = document.getElementById('menu');
 const ul = document.querySelector('.sections-container');
 const togglers = document.querySelectorAll('.toggler');
+
 // Template for the menu source
 const menuTemplate = '/public/svg/';
 
 // Function to toggle the checkbox
 
 const toggleCheckbox = () => {
-  if (window.innerWidth <= 858) {
+  if (window.innerWidth <= 1199.98) {
     if (menuOpen) {
       menuOpen = false;
       menu.src = menuSvg;
@@ -30,7 +29,7 @@ const toggleCheckbox = () => {
 }
 
 
-
+// Event listeners
 togglers.forEach(toggler => {
   toggler.addEventListener('click', toggleCheckbox);
 });
