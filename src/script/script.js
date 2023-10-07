f0f6e3735b7c33ec1a3a529b0b2890bf055706d7
ddmuzyk <domino2k1@gmail.com>
2023-10-07 16:38:00 +0200

    removed the checkbox, addeg a global variable instead

--- src/script/script.js
+++ src/script/script.js
@@ -1,25 +1,37 @@
 import menuSvg from '../../public/svg/menu.svg';
 import xSvg from '../../public/svg/x.svg';
 
+// Remove the checkbox input from the DOM, use a variable instead
 
-const checkbox = document.getElementById('check');
+// Global variables
+let menuOpen = false;
+
+const checkbox = document.querySelector('.checkbtn');
 const menu = document.getElementById('menu');
-
+const ul = document.querySelector('.sections-container');
+const togglers = document.querySelectorAll('.toggler');
 // Template for the menu source
 const menuTemplate = '/public/svg/';
 
 // Function to toggle the checkbox
 
 const toggleCheckbox = () => {
-  if (checkbox.checked) {
-    checkbox.checked = false;
-    menu.src = menuSvg;
-  } else {
-    menu.src = xSvg;
+  if (window.innerWidth <= 858) {
+    if (menuOpen) {
+      menuOpen = false;
+      menu.src = menuSvg;
+      ul.classList.remove('menuOpen')
+    } else {
+      menuOpen = true;
+      menu.src = xSvg;
+      ul.classList.add('menuOpen');
+    }
   }
-
 }
 
-const toggler = document.querySelector('.toggler');
 
-toggler.addEventListener('click', toggleCheckbox);
+
+togglers.forEach(toggler => {
+  toggler.addEventListener('click', toggleCheckbox);
+});
+checkbox.addEventListener('click', toggleCheckbox);
